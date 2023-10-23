e2ac3a5ff2640cc105907f034613f5122791dd82
Olzhas <olzhasabukhan2003@gmail.com>
2023-10-23 10:26:54 +0600

    Добавьте файлы проекта.

---
+++ modul7.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7._2
+{
+    public class MyClass
+    {
+        private int[] array;
+
+        public MyClass(int[] arr)
+        {
+            array = arr;
+        }
+
+        // Сумма элементов массива
+        public int Sum()
+        {
+            return array.Sum();
+        }
+
+        public static bool operator <(MyClass a, MyClass b)
+        {
+            return a.Sum() < b.Sum();
+        }
+
+        public static bool operator >(MyClass a, MyClass b)
+        {
+            return a.Sum() > b.Sum();
+        }
+    }
+}
